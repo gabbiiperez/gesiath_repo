505e0dcf010b03f5dcebf30dd91ea5b5974cc761
Santiago Perez <santy.p510@gmail.com>
2025-01-16 20:43:15 -0300

    Arreglando errores

---
+++ videoclub_santiago/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "videoclub_santiago",
+    "version": "17.0.0.0.1",
+    "summary": "Gestion de peliculas para un videoclub",
+    "category": "Movies",
+    "author": "Santiago Perez",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/movie_data.xml",
+        "views/menu_views.xml",
+        "views/movie_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
