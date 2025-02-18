b90efd0a3687f8c413f352fd2352e1095c972de6
Santiago Perez <santy.p510@gmail.com>
2025-02-17 21:29:29 -0300

    Test reports

---
+++ customer_info_clase13/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Customer Info clase 13",
+    "version": "17.0.0.0.1",
+    "summary": "Test of Qweb",
+    "category": "Uncategorized",
+    "author": "Santiago Perez",
+    "depends": ["contacts"],
+    "data": [
+        "reports/res_partner_report.xml",
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
