4ff052366a60c5f4e6b1b201d9528b2443b7af67
Santiago Perez <santy.p510@gmail.com>
2025-02-20 20:23:10 -0300

    Test of translations

---
+++ customer_info_claseA1/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Customer Info Class A1",
+    "version": "17.0.0.0.1",
+    "summary": "Test of translations",
+    "category": "Uncategorized",
+    "author": "Santiago Perez",
+    "depends": ["contacts"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
