78a64127dce1d9b4360bf977655d45350320c17f
Santiago Perez <santy.p510@gmail.com>
2025-03-06 21:22:57 -0300

    Clase Adicional 4 y su tarea

---
+++ course_ca4/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Course CA4",
+    "version": "17.0.0.0.1",
+    "summary": "Clase adicional 4",
+    "category": "Uncategorized",
+    "author": "Santiago Perez",
+    "depends": ["library"],
+    "data": [
+        "views/course_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
